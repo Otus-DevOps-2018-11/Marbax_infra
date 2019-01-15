a8837ca3dd87d4842af5a579138af25e7d52c9f1
Nikita Morozov <marbax117@gmail.com>
2019-01-15 22:23:22 +0200

    HW7 complete with all tasks

---
+++ config-scripts/create-reddit-vm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+gcloud compute instances create reddit-app\
+  --boot-disk-size=10GB \
+  --image=reddit-base-1547582274 \
+  --image-project=infra-226316 \
+  --machine-type=g1-small \
+  --tags puma-server \
+  --restart-on-failure \
+
+
